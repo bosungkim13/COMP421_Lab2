e37f5ccfdbf2906336c3b64b9589aa30ebba2b5e
bosungkim13 <89793411+bosungkim13@users.noreply.github.com>
2023-03-06 13:52:42 -0600

    Create memoryManagement.c

---
+++ memoryManagement.c
@@ -0,0 +1,36 @@
+// include headers as needed
+
+// Initialize integer array to keep track of page status (0 free, 1 used)
+int *isPhysicalPageOccupied = NULL;
+int numPhysicalPages;
+void *kernel_break = (void *)VNEM_1_BASE;
+
+void initPhysicalPageArray(unsigned int pmem_size){
+    // keep track of page status (0 free, 1 used)
+    numPhysicalPages = pmem_size/PAGESIZE;
+    isPhysicalPageOccupied = malloc(numPhysicalPages * sizeof(int));
+    memset(isPhysicalPageOccupied, 0, numPhysicalPages);
+}
+// helper function to return number of free phyical pages
+int freePhysicalPageCount(){
+    int count = 0;
+    int i;
+
+    for (i = 0; i < numPhysicalPages; i++){
+        if (isPhysicalPageOccupied[i] == 0){
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+// helper function to mark the pages in the given ranges as occupied
+void markPagesInRange(void *start, void *end){
+    int i;
+    int begin = DOWN_TO_PAGE(start) / PAGESIZE;
+    int limit = UP_TO_PAGE(end) / PAGESIZE;
+
+    for (i = begin; i < limit; i++){
+        isPhysicalPageOccupied[i] = 1;
+    }
+}
